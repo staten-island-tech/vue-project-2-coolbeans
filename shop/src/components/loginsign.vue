a13486f22e4f8274a9b48c365d7fdaa75e5d5afc
Blanku-cloud <pokemondinner321@gmail.com>
2022-03-10 21:42:02 -0500

    started login

---
+++ shop/src/components/loginsign.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="box">
+    <h2 class="head"><slot name="head"></slot></h2>
+    <h3 class="subhead" id="subhead1"><slot name="subhead1"></slot></h3>
+    <h3 class="subhead" id="subhead2"><slot name="subhead2"></slot></h3>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  width: 50.625rem;
+  height: 42.75rem;
+  background-color: black;
+}
+</style>
