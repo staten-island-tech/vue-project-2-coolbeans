9ec348899e60be505326af10a7dc35efb955a583
Blanku-cloud <pokemondinner321@gmail.com>
2022-03-16 10:12:30 -0400

    practing

--- shop/src/views/HomeView.vue
+++ shop/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
+<script></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>home</h1>
+  </div>
 </template>
