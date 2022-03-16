e1eacad710e25327a73972e078bd274bb2db48da
Blanku-cloud <pokemondinner321@gmail.com>
2022-03-16 09:33:25 -0400

    Revert "Added Vuex"
    This reverts commit 1879a4bf9b7d257381ecdc0a8808e33b4bf629a8.

--- shop/src/main.js
+++ shop/src/main.js
@@ -1,10 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(router);
-app.use(store);
-app.mount("#app");
+app.use(router)
+
+app.mount('#app')
