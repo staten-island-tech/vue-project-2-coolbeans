13afd66dd2fd1d4476181757944a2d32d0eedc4c
Blanku-cloud <pokemondinner321@gmail.com>
2022-05-23 09:59:14 -0400

    Update places.vue

--- shop/src/views/places.vue
+++ shop/src/views/places.vue
@@ -1,38 +1,18 @@
 <template>
-  <div class="wrapper">
-    <Posticon
-      class="post"
-      v-for="(post, index) in posts"
-      :key="index"
-      :title="post.title"
-      :image="post.image"
-    />
-  </div>
+  <div class="wrapper"></div>
 </template>
 
 <script>
 import Button from "../components/Button.vue";
-import Posticon from "../components/Posticon.vue";
 
 export default {
   name: "Home",
   components: {
     Button,
-    Posticon,
   },
   methods: {},
-  computed: {
-    posts() {
-      return this.$store.state.loadPost;
-    },
-  },
-  created() {
-    axios
-      .get("https://auth-shop-994d6-default-rtdb.firebaseio.com/posts.json")
-      .then(function (data) {
-        console.log(data);
-      });
-  },
+  computed: {},
+
   data() {
     return {};
   },
