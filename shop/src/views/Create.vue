1f2d188e81f4ee4885cc61cc050d18c396091245
jasonchen3805 <90630687+jchn3805@users.noreply.github.com>
2022-05-29 09:03:06 -0400

    Update Create.vue

--- shop/src/views/Create.vue
+++ shop/src/views/Create.vue
@@ -58,7 +58,8 @@
           <!-- <button :disabled="!isFormValid" type="submit">Post</button> -->
         </form>
       </div>
-      <div v-show="isPreview" class="preview">
+      <!-- v-show="isPreview" -->
+      <div class="preview">
         <div class="card">
           <div class="card-image" v-show="isPicValid">
             <img :src="pickImage || imageUrl" alt="" />
